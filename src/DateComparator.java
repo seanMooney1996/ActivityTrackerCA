853e2a42d467675bfe61357f63e64c798262a1db
seanMooney1996 <seanmooney1996@gmail.com>
2023-10-18 19:16:06 +0100

    Added ascending/descending  options to duration, date and distance

--- src/DateComparator.java
+++ src/DateComparator.java
@@ -5,16 +5,31 @@ import java.util.Date;
 
 public class DateComparator implements Comparator<ActivityEntry>{
     private SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-
+    boolean ascending = true;
+    public DateComparator(boolean ascending) {
+        this.ascending = ascending;
+    }
     @Override
     public int compare(ActivityEntry entry1, ActivityEntry entry2) {
-        try {
-            Date date1 = dateFormat.parse(entry1.getDate());
-            Date date2 = dateFormat.parse(entry2.getDate());
+        Date date1;
+        Date date2;
+        if (ascending) {
+            try {
+                date1 = dateFormat.parse(entry1.getDate());
+                date2 = dateFormat.parse(entry2.getDate());
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
             return date2.compareTo(date1);
-        } catch (ParseException e) {
-            e.printStackTrace(); // Handle the exception appropriately
-            return 0; // or throw an exception or return a default value
+        } else {
+            try {
+                date1 = dateFormat.parse(entry1.getDate());
+                date2 = dateFormat.parse(entry2.getDate());
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+            return date1.compareTo(date2);
         }
+
     }
 }
